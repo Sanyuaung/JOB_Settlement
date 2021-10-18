a75fc4a4e131d1e6b12a19414855c741aab98860
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2021-10-18 12:57:28 +0630

    Merchant Name Update

--- app/Models/inc01c.php
+++ app/Models/inc01c.php
@@ -22,13 +22,13 @@ class inc01c extends Model
         substring(Field1,83,6) as Authorization_Identification_Response,concat(substring(Field1,89,2),"/",substring(Field1,91,2)) as Date_Authorization,
         substring(Field1,93,12) as RRN,substring(Field1,105,11) as Acquring_IIN,substring(Field1,116,11) as Forwarding_IIN,
         substring(Field1,127,4) as Merchant_Type,substring(131,8) as Card_Acceptor_Terminal_Identification,
-        B.MERCHANT_ID AS Card_Acceptor_Identification,B.MERCHANT_TRADE_NAME as Card_Acceptor_Name,
+        substring(Field1,139,15) AS Card_Acceptor_Identification,B.MERCHANT_TRADE_NAME as Card_Acceptor_Name,
         substring(Field1,194,23) as Original_Transaction_Information,substring(Field1,217,4) as Message_Reason_Code,
         substring(Field1,221,11) as Receiving_IIN,substring(Field1,232,11) as Issuing_IIN,substring(Field1,243,1) as Identifer_of_Transaction_Feature,
         substring(Field1,244,2) as Point_of_Service_Condition_Code,substring(Field1,246,3) as Merchant_Country_Code,
         substring(Field1,249,3) as Authorization_Type,format(concat(substring(Field1,252,10),".",substring(Field1,262,2)),2) as Service_Fee_Receivable,
         format(concat(substring(Field1,264,10),".",substring(Field1,274,2)),2) as Service_Fee_Payable,
-        substring(Field1,276,5) as Reserved from inc01cs A inner join merchants B WHERE substring(Field1,139,15) = B.MERCHANT_ID');
+        substring(Field1,276,5) as Reserved from inc01cs A left join merchants B on substring(Field1,139,15) = B.MERCHANT_ID');
         return $inc01c;
     }
 }
