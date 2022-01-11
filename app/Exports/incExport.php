1c7b200692a60b81459d61d0ee592003829be9e6
San Yu Aung <sanyuaung.ygn.mm@gmail.com>
2022-01-11 12:41:54 +0630

    add

--- app/Exports/incExport.php
+++ app/Exports/incExport.php
@@ -27,7 +27,7 @@ class incExport implements FromCollection, WithHeadings, WithMapping
             'STF_amt',
             'STF_Fee_sign',
             'STF_fee',
-            'outgoing_sum</th',
+            'outgoing_sum',
             'incoming_summary',
             'settlement_curr',
             'reserved',
